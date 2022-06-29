2ec436507266cf016747a36499052e7efb3835f8
ligenxxxx <59721724+ligenxxxx@users.noreply.github.com>
2022-06-29 13:08:08 +0800

    load ch341DLL

--- ch341.py
+++ ch341.py
@@ -2,8 +2,12 @@ import os
 import time
 from ctypes import *
 
-class USB():
-    ch341dllPath = './dll/CH341DLL.LIB'
+def ch341LoadDLL():
+    ch341dllPath = './dll/CH341DLL.DLL'
     ch341 = windll.LoadLibrary(ch341dllPath)
+    return ch341
 def ch341ThreadProc():
+    ch341 = ch341LoadDLL()
     print('start ch341ThreadProc')
+    print(ch341)
+    
