e1ed68782e8838e40cb7d6e1c97f56d6911a35df
ligenxxxx <ligenxxxx@gmail.com>
2023-03-04 11:56:04 +0800

    add icon_convert

---
+++ icon_convert.py
@@ -0,0 +1,9 @@
+import base64
+
+# 读取图像文件并编码为Base64字符串
+with open("32.ico", "rb") as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+# 将Base64编码字符串写入Python文件中
+with open("icon32.py", "w") as python_file:
+    python_file.write("icon32 = '{}'\n".format(encoded_string))
